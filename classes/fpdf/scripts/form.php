e345e7c7cf550b3a99d66cf20e430dd47c4c1bc0
Shawn Gibbons <sgibbons@palmettoimages.com>
2015-08-13 13:36:28 -0400

    updates

--- classes/fpdf/scripts/form.php
+++ classes/fpdf/scripts/form.php
@@ -42,6 +42,9 @@ class FormPDF extends GridPDF {
 	public function _AddPage($page, $orientation='P', $size='letter') {
 
 		$this->AddPage($orientation, $size);
+		echo '<pre>';
+		var_dump($this->_pages);
+		echo '</pre>';
 	    foreach($this->_pages->$page->fields as $field) {
 	    	if($field->type != 'table') {
 	    		$this->{$field->type}($field);
