192e6573c1967ba836fce74279092cf4d6456fc4
Shawn Gibbons <sgibbons@palmettoimages.com>
2015-08-13 11:31:41 -0400

    updates

--- classes/fpdf/scripts/form.php
+++ classes/fpdf/scripts/form.php
@@ -133,6 +133,9 @@ class FormPDF extends GridPDF {
 
 	}
 	public function textbox($field) {
+		echo '<pre>';
+		var_dump($field);
+		echo '</pre>';
 		$params = $field->params;
 		$this->SetXY($params->x, $params->y);
 		//var_dump($params);
