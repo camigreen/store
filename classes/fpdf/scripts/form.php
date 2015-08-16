bf19ca8036e83e713c619158309ec9e59568504e
Shawn Gibbons <sgibbons@palmettoimages.com>
2015-08-16 11:27:22 -0400

    updates

--- classes/fpdf/scripts/form.php
+++ classes/fpdf/scripts/form.php
@@ -148,9 +148,6 @@ class FormPDF extends GridPDF {
 		$col_x = $field->x;
 		$col_y = $field->y;
 		$data = $this->arrangeItems($field, $this->order_data[$field->name]);
-		echo '<pre>';
-		var_dump($data);
-		echo '</pre>';
 		foreach($field->columns as $column) {
 			$w = $field->w*$column->w;
 			$this->SetXY($col_x,$col_y);
@@ -318,9 +315,9 @@ class FormPDF extends GridPDF {
 			'columns' => array()
 		);
 	    $last_row = 0;
-	 //    echo '<pre>';
-		// var_dump($data);
-		// echo '</pre>';
+	    echo '<pre>';
+		var_dump($data);
+		echo '</pre>';
 	    foreach ($data as $item) {
 	    	$starting_row = $last_row;
 	    	foreach($item as $column => $value) {
@@ -343,9 +340,9 @@ class FormPDF extends GridPDF {
 	    }
 	    $table['total_rows'] = $last_row;
 	    
-		// echo '<pre>';
-		// var_dump($table);
-		// echo '</pre>';
+		echo '<pre>';
+		var_dump($table);
+		echo '</pre>';
 
 		return $table;
 
