bf302a2eaa03541f8e41f73394a777bfc6d2429f
Shawn Gibbons <sgibbons@palmettoimages.com>
2015-08-16 15:56:00 -0400

    updates

--- classes/fpdf/scripts/form.php
+++ classes/fpdf/scripts/form.php
@@ -343,7 +343,7 @@ class FormPDF extends GridPDF {
 					$columns = $field->columns;
 					if(isset($v['format'])) {
 						$format = $this->form->formats->get($v['format'], $this->app->data->create());
-						$indent = $format->get('indent', 0));
+						$indent = $format->get('indent', 0);
 					}
 					$lines = $this->NbLines(($field->w*$columns->$column->w)-$indent,$v['text']);
 			    	foreach($lines as $line) {
