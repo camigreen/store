5863018fb314ec99156a13d1c0ef77bf91cba475
Shawn Gibbons <sgibbons@palmettoimages.com>
2015-08-16 11:53:29 -0400

    updates

--- classes/fpdf/scripts/form.php
+++ classes/fpdf/scripts/form.php
@@ -170,7 +170,7 @@ class FormPDF extends GridPDF {
 				}
 				$border = implode(',',$b);
 				$b = array();
-				echo (($rows - 1) == $i).'</br>';
+				echo $i.':'.$rows.'</br>';
 				echo $border.'</br>';
 				$this->format($column);
 				$text = isset($data['columns'][$i][$column->name]['text']) ? $data['columns'][$i][$column->name]['text'] : '';
