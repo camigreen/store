9d1f9379f57cffd92b705221ca218464c829d48e
Shawn Gibbons <sgibbons@palmettoimages.com>
2015-08-26 13:02:04 -0400

    updated save function of the account controller

--- templates/ttop/accounts.php
+++ templates/ttop/accounts.php
@@ -19,9 +19,11 @@ $modified = $this->app->date->create($this->account->modified)->format('m/d/Y g:
 		<input type="text" name="account[number]" value="<?php echo $this->account->number; ?>" />
 		<input type="text" name="account[type]" value="<?php echo $this->account->type; ?>" />
 		<input type="date" name="account[created]" value="<?php echo $created; ?>" />
-		<input type="text" name="account[created_by]" value="<?php echo $this->app->user->get($this->account->created_by)->name; ?>" />
+		<input type="text" name="created_by_name" value="<?php echo $this->app->user->get($this->account->created_by)->name; ?>" />
+		<input type="hidden" name="account[created_by]" value="<?php echo $this->account->created_by; ?>" />
 		<input type="date" name="account[modified]" value="<?php echo $modified; ?>" />
 		<input type="text" name="account[modified_by]" value="<?php echo $this->app->user->get($this->account->modified_by)->name; ?>" />
+		<input type="hidden" name="modified_by_name" value="<?php echo $this->account->modified_by; ?>" />
 		<input type="text" name="account[params][terms]" value="<?php echo $this->account->params->get('terms', ''); ?>" />
 		<input type="text" name="account[params][discount]" value="<?php echo $this->account->params->get('discount', ''); ?>" />
 		<input type="submit" class="uk-button uk-button-primary" value="Save" />
