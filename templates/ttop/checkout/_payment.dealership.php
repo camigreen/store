083209e5e412a416435abb8a4d8fe9818f5f9024
Shawn Gibbons <sgibbons@palmettoimages.com>
2015-11-12 13:01:31 -0500

    updates

--- templates/ttop/checkout/_payment.dealership.php
+++ templates/ttop/checkout/_payment.dealership.php
@@ -99,6 +99,8 @@ $totals = $order->getTotals();
                         <input type="text" name="elements[payment][account_name]" disabled class="ttop-checkout-field required" value='<?php echo $elements->get('payment.account_name'); ?>'/>
                         <label>Account Number:</label>
                         <input type="text" name="elements[payment][account_number]" disabled class="ttop-checkout-field required" value='<?php echo $elements->get('payment.account_number') ?>'/>
+                        <label>Customer Name</label>
+                        <input type="text" name="elements[payment][customer_name]" class="ttop-checkout-field required" value='<?php echo $elements->get('payment.customer_name') ?>'/>
                         <label>P.O. Number:</label>
                         <input type="text" name="elements[payment][po_number]" class="ttop-checkout-field required" value='<?php echo $elements->get('payment.po_number') ?>'/>
                     </div>
